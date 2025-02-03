9b898c2dcc4226d62fb59ce3c38e43bb95705dfd
PRIYOBRATO.DAS <priyodas12@gmail.com>
2025-02-03 22:41:17 +0530

    basic set up

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-const heading=React.createElement("h1",{className: "heading",style: {color: "red"}},"Hellow React!");
-const footer=React.createElement("h2",{className: "foorter",style: {color: "green"}},"Footer Content!");
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+const heading=React.createElement("h1",{"id":"header"}," React Learning! 🦇");
 const rootEl=ReactDOM.createRoot(document.getElementById("root"));
-rootEl.render(heading);
+
+const anotherHeading=<h2 id='test'>This is JSX!🐎</h2>;
 console.log(heading);
-rootEl.render(React.createElement("div",{className: "container"},[heading,footer]));
+console.log(rootEl);
+console.log(anotherHeading);
+const conatiner=React.createElement("div",{"id":"container"},[heading,anotherHeading]);
+
+rootEl.render(conatiner);
